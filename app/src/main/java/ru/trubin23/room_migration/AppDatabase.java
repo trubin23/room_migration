927afc3561439e9a21a49cbc6d12fbcc683548f9
Trubin Andrey <trubin23@gmail.com>
2019-02-11 23:59:11 +0500

    add Employee entity

--- app/src/main/java/ru/trubin23/room_migration/AppDatabase.java
+++ app/src/main/java/ru/trubin23/room_migration/AppDatabase.java
@@ -3,6 +3,6 @@ package ru.trubin23.room_migration;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {}, version = 2)
+@Database(entities = {Employee.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 }
---
+++ app/src/main/java/ru/trubin23/room_migration/Employee.java
@@ -0,0 +1,15 @@
+package ru.trubin23.room_migration;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class Employee {
+
+    @PrimaryKey
+    public long id;
+
+    public String name;
+
+    public int salary;
+}
