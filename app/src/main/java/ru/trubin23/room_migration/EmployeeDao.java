eaa2c1c5f00c73b72757956711894938f99b3fa3
Trubin Andrey <trubin23@gmail.com>
2019-02-12 23:58:02 +0500

    add queries to EmployeeDao

--- app/src/main/java/ru/trubin23/room_migration/EmployeeDao.java
+++ app/src/main/java/ru/trubin23/room_migration/EmployeeDao.java
@@ -1,7 +1,10 @@
 package ru.trubin23.room_migration;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 
@@ -10,4 +13,22 @@ public interface EmployeeDao {
 
     @Query("SELECT * FROM employee")
     List<Employee> getAll();
+
+    @Query("SELECT * FROM employee ORDER BY salary DESC")
+    List<Employee> getAllOrderBySalary();
+
+    @Insert
+    void insert(Employee employee);
+
+    @Insert
+    void insertAll(List<Employee> employees);
+
+    @Update
+    int update(Employee employee);
+
+    @Delete
+    void delete(Employee employee);
+
+    @Query("DELETE FROM employee")
+    void deleteAll(Employee employee);
 }
